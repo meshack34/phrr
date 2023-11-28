59b8a4e9521f936ca03f91a4012c31519911d550
meshack34 <meshackkimutai34@gmail.com>
2023-11-28 06:54:38 -0500

    update

--- api/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .serializers import *
-from rest_framework_simplejwt.views import TokenObtainPairView
-# Create your views here.
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
